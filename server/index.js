09f3920ae10d66f9f99a47657b0ec8f10cccabd5
zhixiam <mot666888@gmail.com>
2022-06-30 07:55:01 +0800

    origin main

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(post, () => console.log(`Server started on post ${port}`))
