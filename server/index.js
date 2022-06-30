049362abc704a4fe7910396af0f9e7e2fabb546b
zhixiam <mot666888@gmail.com>
2022-06-30 08:40:26 +0800

    go

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const app = express();
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
+// Handle production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + "/public"));
+    app.get(/.*/);
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on post ${port}`))
